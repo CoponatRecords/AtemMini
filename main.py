2d7264d30cbef4241f91d89977abd5bb5a5df2d9
Coponat Records <talent@coponatrecords.com>
2023-01-22 20:35:15 +0100

    Atem mini automatic switcher

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-# Automatic Scene Switcher for atem mini
+'''
+Automatic Scene Switcher for Atem Mini
+
+ToDo :
+    read ableton audio levels for smart switching
+    add chat/remote input
+    add on/off and camera number gui
+'''
 
 import time
 import PyATEMMax
@@ -16,12 +23,10 @@ print("Connected")
 def camera(n): #Switches the camera
     return switcher.setProgramInputVideoSource(0, n)
 
-def rotate_camera(list_of_cameras, time_sleep): #Selects camera input at random
+def rotate_camera(list_of_cameras, time_sleep): #Selects camera input at random, time_sleep is how much time it takes to switch cameras
     n = random.choice(list_of_cameras)
     camera(n)
     print("Current Camera:  "+str(n))
     time.sleep(time_sleep)
-
-
 while True:
     rotate_camera([1,2,3], 2)
