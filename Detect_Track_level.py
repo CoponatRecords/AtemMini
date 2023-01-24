b68f6116b1542f87128668f0f1f8b6717e5383a2
Coponat Records <talent@coponatrecords.com>
2023-01-24 01:14:04 +0100

    Atem mini automatic switcher

--- Detect_Track_level.py
+++ Detect_Track_level.py
@@ -1,27 +1,33 @@
 from pythonosc import dispatcher
 from pythonosc import osc_server
 from pythonosc import udp_client
-import concurrent.futures
+import time
+import threading
+import time
 
 # OSC client for sending messages to Ableton
 client = udp_client.SimpleUDPClient("127.0.0.1", 11000)
 
-# Request the volume of the first track
-client.send_message("/live/device/get/parameters/value", [0,2])
-
 # Create a dispatcher
 disp = dispatcher.Dispatcher()
 
-# Register a function to handle OSC messages on address "/live/volume"
+# Register a function to handle OSC messages on address "/live/device/get/parameters/value"
 def volume_handler(unused_addr, *args):
-    for name, value in zip(args[0::2], args[1::2]):
-        print("Parameter name: {0}, value: {1}".format(name, value))
+    print("Volume of track "+str(args[-3])) #found empiricaly
 
 disp.map("/live/device/get/parameters/value", volume_handler)
 
 # Create a OSC server
 server = osc_server.ThreadingOSCUDPServer(("127.0.0.1", 11001), disp)
 # Start the OSC server
-server.serve_forever()
+server_thread = threading.Thread(target=server.serve_forever)
+server_thread.start()
+
+while True:
+    # Request the parameters' values of the device at index 2
+    client.send_message("/live/device/get/parameters/value", [0, 2])
+    # Wait for 1 second
+    time.sleep(0.1)
+
 server.shutdown()
 print("Done")
