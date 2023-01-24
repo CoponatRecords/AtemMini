effb67f684b999a042463ab9962b546a59e0d97a
Coponat Records <talent@coponatrecords.com>
2023-01-24 20:44:14 +0100

    Atem mini automatic switcher

--- Instrument_Level.py
+++ Instrument_Level.py
@@ -5,44 +5,51 @@ import threading
 import time
 import logging
 
-#This script write 0 or 1 in an instrument_status file if the instruments are generating sound
+'''
+This script writes in an instrument_status.txt file and piano_status.txt the current audio level of the tracks.
+Works with an envelope follower linked to a utility's gain on the third (group all instrument) and fourth (piano with a 
+vst) tracks of ableton
+
+Ableton needs to have AbletonOSC installed : https://github.com/ideoforms/AbletonOSC
+To run this script : right-click, run 'instrument_level'
+'''
 
 # OSC client for sending messages to Ableton
 client = udp_client.SimpleUDPClient("127.0.0.1", 11000)
 # Create a dispatcher
 disp = dispatcher.Dispatcher()
 
+
 # Register a function to handle OSC messages on address "/live/device/get/parameters/value"
-def volume_handler(unused_addr, *args):
-    #print("Volume of track "+str(args[-3])) #found empiricaly
-    print(float(args[-3]))
+def volume_handler(*args):
+    # print("Volume of track "+str(args[-3])) #found empiricaly
 
     if n == 0:
         with open("instrument_status.txt", "w") as file:
             file.write(str(args[-3]))
+            print('Instru\t- ' + str(float(args[-3])))
+
     elif n == 1:
         with open("piano_status.txt", "w") as file:
             file.write(str(args[-3]))
+            print('Piano\t- ' + str(float(args[-3])))
 
 
 disp.map("/live/device/get/parameters/value", volume_handler)
-
 # Create a OSC server
 server = osc_server.ThreadingOSCUDPServer(("127.0.0.1", 11001), disp)
 # Start the OSC server
 server_thread = threading.Thread(target=server.serve_forever)
 server_thread.start()
 
-
 n = 0
 while True:
     # Request the parameters' values of the device, alternate between all instruments and just piano
-    if n == 0 :
-        client.send_message("/live/device/get/parameters/value", [3,2])
+    if n == 0:
+        client.send_message("/live/device/get/parameters/value", [3, 2])
         n = 1
     else:
-        client.send_message("/live/device/get/parameters/value", [2,1])
+        client.send_message("/live/device/get/parameters/value", [2, 1])
         n = 0
 
     time.sleep(0.5)
-
--- main.py
+++ main.py
@@ -2,32 +2,48 @@
 Automatic Scene Switcher for Atem Mini
 
 ToDo :
-
     add  gui
     regroup both scripts on one start button
 '''
 
+'''
+This script reads the instrument_status.txt file and piano_status.txt to switch cameras
+The Atem mini has to be on and connected through ethernet. Check the connection status with the "Atem Setup" app.
+The Instrument_Level script has to be on 
+Ableton needs to have AbletonOSC installed : https://github.com/ideoforms/AbletonOSC
+
+To run this script : right-click, run 'main'
+
+'''
+
+import PySimpleGUI as sg
+
 import time
 import PyATEMMax
 import random
-
-def piano_status():
-    with open("piano_status.txt", "r") as file:
-        content = file.read()
-        a = float(content)
-    return a
-
-
-def track_status():
-    with open("instrument_status.txt", "r") as file:
-        content = file.read()
-        a = float(content)
-    return a
-
 switcher = PyATEMMax.ATEMMax()
 
-def connection_to_switcher():
+def piano_level():
+    try:
+        with open("piano_status.txt", "r") as file:
+            content = file.read()
+            return float(content)
 
+    except:
+        print("Error in piano_level, returning 0")
+        return 0
+
+def instrument_group_level():
+    try:
+        with open("instrument_status.txt", "r") as file:
+            content = file.read()
+            return float(content)
+    except:
+        print("Error in instrument_group_level, returning 0")
+        return 0
+
+
+def connection_to_switcher():
     # Connect
     atem_mini_ip = "192.168.0.124"
 
@@ -41,28 +57,45 @@ def connection_to_switcher():
 connection_to_switcher()
 
 def camera(n,switcher): #Switches the camera
-    return switcher.setProgramInputVideoSource(0, n)
+    try:
+        switcher.setProgramInputVideoSource(0, n)
+    except:
+        print("Error in camera()")
+    return
 
 def rotate_camera(list_of_cameras,switcher): #Selects camera input at random, time_sleep is how much time it takes to switch cameras
     n = random.choice(list_of_cameras)
     camera(n,switcher)
     return str(("Current Camera:  "+str(n)))
 
-
 while True:
-    sleep_time = 8
-    if track_status() > 0.005: #If there's audio in the instruments group, then rotate cameras
 
-        if piano_status() > 0.005:
+    sleep_time = 8
+    if instrument_group_level() > 0.005: #If there's audio in the instruments group, then rotate cameras
+
+        if piano_level() > 0.005:
             print("Instruments Playing - With Piano - Rotating Cameras 123 - " + rotate_camera([1,2,3],switcher))
+
+            for k in range(0, sleep_time):
+                time.sleep(1)
+                if piano_level() < 0.005:
+                    print("Piano Stopped !")
+                    break
+
         else:
             print("Instruments Playing - No Piano - Rotating Cameras - 23 -" + rotate_camera([2,3],switcher))
 
-        time.sleep(sleep_time)
-
+            for k in range(0, sleep_time):
+                time.sleep(1)
+                if instrument_group_level() < 0.005:
+                    print("Instruments Off !")
+                    break
+                elif piano_level() > 0.005:
+                    print("Piano just came in !")
+                    break
 
     else:
-        print("Instruments Off - Camera on Face")
+        print("Instruments Off - Camera on Face - Camera 2")
         camera(2,switcher)
         time.sleep(1)
 
