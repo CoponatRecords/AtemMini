a7016db4759203be59b0a94faf293184ec9ad534
Coponat Records <talent@coponatrecords.com>
2023-01-28 13:53:07 +0100

    Atem mini automatic switcher

--- main.py
+++ main.py
@@ -16,11 +16,9 @@ To run this script : right-click, run 'main'
 
 '''
 
-from plyer import notification
 import time
 import PyATEMMax
 import random
-from notifypy import Notify
 
 #used to add color in terminal
 CRED_RED = '\033[91m'
@@ -31,11 +29,18 @@ CRED_GREEN_2 = '\033[92m'
 CRED_ORANGE = '\033[43m'
 CEND= '\033[0m'
 
+
+def current_time():
+    '''
+    :return: time in hh:mm:ss format
+    '''
+    t = time.localtime()
+    return str(time.strftime("%H:%M:%S", t)+' ')
+
 #used as global variable to print less stuff in the terminal
 camera_face = 1
 wide_angle = 2
 zoom = 3
-
 
 #time to wait before trying to switch cameras
 sleep_time = 8
@@ -43,26 +48,39 @@ sleep_time = 8
 
 #Functions to communicate with the Atem Mini
 def connection_to_switcher():
+    '''
+    :return: connects to the switcher
+    '''
     # Connect
     atem_mini_ip = "192.168.0.124"
 
-    print(CRED_RED+"Connecting to atem mini"+CEND)
+    print(current_time()+CRED_RED+" Connecting to atem mini"+CEND)
     print("Remember to launch the other script for automatic switching with sound detection")
 
     switcher.connect(atem_mini_ip)
     switcher.waitForConnection()
-    print(CRED_RED+"Connected to atem mini"+CEND)
-
+    print(current_time()+CRED_RED+" Connected to atem mini"+CEND)
 
 def camera(n,switcher): #Switches the camera
+    '''
+    :param n: the camera number
+    :param switcher: the switcher
+    :return: Changes the cameras
+    '''
     try:
         switcher.setPreviewInputVideoSource(0, n)
         switcher.execAutoME(0)
-
     except:
         print(CRED_RED+"Error in camera()"+CEND)
 
-def rotate_camera(list_of_cameras,switcher): #Selects camera input at random, time_sleep is how much time it takes to switch cameras
+def rotate_camera(list_of_cameras,switcher):
+
+    '''
+    :param list_of_cameras: The cameras that will be used for the rotation
+    :param switcher: the atem mini switcher
+    :return: Selects camera input at random, time_sleep is how much time it takes to switch cameras
+    '''
+
     n = random.choice(list_of_cameras)
     camera(n,switcher)
 
@@ -71,55 +89,61 @@ def rotate_camera(list_of_cameras,switcher): #Selects camera input at random, ti
 switcher = PyATEMMax.ATEMMax()
 connection_to_switcher()
 
-#Functions to read the files created by Instrument_Level.py
+
 def piano_level():
+    '''
+    :return: reads the file piano_status.txt created by Instrument_Level.py
+    '''
     try:
         with open("piano_status.txt", "r") as file:
             content = file.read()
             return float(content)
-
     except:
-        print("Error in piano_level, returning 0")
+        print(current_time()+"Error in piano_level, returning 0")
         return 0
 
 def instrument_group_level():
+    '''
+    :return: reads the file instrument_status.txt created by Instrument_Level.py
+    '''
     try:
         with open("instrument_status.txt", "r") as file:
             content = file.read()
             return float(content)
     except:
-        print(CRED_RED+"Error in instrument_group_level, returning 0"+CEND)
+        print(current_time()+CRED_RED+" Error in instrument_group_level, returning 0"+CEND)
         return 0
 
 while True:
+
     if instrument_group_level() > 0.005: #If there's audio in the instruments group, then rotate cameras
         camera_face = 1
 
         if piano_level() > 0.0005:
-            print(CRED_BLUE_2+' '+CEND+" Instruments Playing - With Piano - Rotating Cameras 123 -"+CRED_GREEN_2+" Current Camera: " +rotate_camera([1,2,3],switcher)+CEND)
+            print(current_time()+CRED_BLUE_2+' '+CEND+" Instruments Playing - With Piano - Rotating Cameras 123 -"+CRED_GREEN_2+" Current Camera: " +rotate_camera([1,2,3],switcher)+CEND)
 
             for k in range(0, sleep_time):
                 time.sleep(1)
                 if piano_level() < 0.0005:
-                    print(CRED_BLUE_2+' '+CEND+CRED_BLUE+" Piano Stopped !"+CEND)
+                    print(current_time()+CRED_BLUE_2+' '+CEND+CRED_BLUE+" Piano Stopped !"+CEND)
                     break
 
         else:
-            print(CRED_GREEN+' '+CEND+" Instruments Playing - No Piano - Rotating Cameras - 23 - "+CRED_GREEN_2+'Current Camera: '+rotate_camera([2,3],switcher)+CEND)
+            print(current_time()+CRED_GREEN+' '+CEND+" Instruments Playing - No Piano - Rotating Cameras - 23 - "+CRED_GREEN_2+'Current Camera: '+rotate_camera([2,3],switcher)+CEND)
 
             for k in range(0, sleep_time):
                 time.sleep(1)
                 if instrument_group_level() < 0.0005:
-                    print(CRED_BLUE_2+' '+CEND+CRED_BLUE+" Instruments Off !"+CEND)
+                    print(current_time()+CRED_BLUE_2+' '+CEND+CRED_BLUE+" Instruments Off !"+CEND)
                     break
                 elif piano_level() > 0.0005:
-                    print(CRED_BLUE_2+' '+CEND+ CRED_BLUE+" Piano just came in !"+CEND)
+                    print(current_time()+CRED_BLUE_2+' '+CEND+ CRED_BLUE+" Piano just came in !"+CEND)
                     break
 
     else:
 
         if camera_face == 1:
-            print(CRED_ORANGE+' '+CEND+" Instruments Off - Camera on Face - Camera 2")
+            print(current_time()+CRED_ORANGE+' '+CEND+" Instruments Off - Camera on Face - Camera 2")
             camera_face = 0
 
         camera(2,switcher)
